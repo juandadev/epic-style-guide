9f1852cc77e0de1ecdadc291043ced0be0eac9e4
Juan Daniel Martinez <mtzn_juandaniel@yahoo.com.mx>
2021-04-26 03:06:24 -0600

    Completed the theme switcher function

--- script.js
+++ script.js
@@ -26,7 +26,31 @@ function toggleDarkLight(theme) {
   document.querySelector("html").style.setProperty("--text_color", textColor);
 }
 
+function switchTheme(theme) {
+  for (let i = 0; i < theme.length; i++) {
+    document
+      .querySelector("html")
+      .style.setProperty(`--${theme[i][0]}`, theme[i][1]);
+  }
+}
+
 window.addEventListener("load", () => {
+  document
+    .querySelector(".themes__item.theme1")
+    .addEventListener("click", () => switchTheme(theme1));
+  document
+    .querySelector(".themes__item.theme2")
+    .addEventListener("click", () => switchTheme(theme2));
+  document
+    .querySelector(".themes__item.theme3")
+    .addEventListener("click", () => switchTheme(theme3));
+  document
+    .querySelector(".themes__item.theme4")
+    .addEventListener("click", () => switchTheme(theme4));
+  document
+    .querySelector(".themes__item.theme5")
+    .addEventListener("click", () => switchTheme(theme5));
+
   document
     .querySelector(".theme-light")
     .addEventListener("click", () => toggleDarkLight("light"));
